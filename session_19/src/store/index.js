ffe2df872ff8991fdfd74f5b6b3b122b1b996204
hank666777 <fengptt47@gmail.com>
2024-05-23 01:10:48 +0800

    feat: temporary storage

--- session_19/src/store/index.js
+++ session_19/src/store/index.js
@@ -1,5 +1,18 @@
 import { configureStore, createSlice } from '@reduxjs/toolkit';
 
+const moviesSlice = createSlice({
+  name: 'movie',
+  initialState: [],
+  reducers: {
+    addMovie(state, action) {
+      state.push(action.payload);
+    },
+    removeMovie(state, action) {
+      state.splice(state.indexOf(action.payload), 1);
+    }
+  }
+})
+
 const songsSlice = createSlice({
   name: 'song',
   initialState: [],
@@ -7,11 +20,12 @@ const songsSlice = createSlice({
     // song + / + addSong = 'song/addSong'
     addSong(state, action) {
       // state is not the big state object in the store
-      // iit is the piece of state managed by this reducer
+      // It is the piece of state managed by this reducer
       state.push(action.payload);
     },
     // song + / + removeSong = 'song/removeSong'
     removeSong(state, action) {
+      state.splice(state.indexOf(action.payload), 1);
     },
   },
 });
@@ -20,6 +34,7 @@ const store = configureStore({
   reducer: {
     // state = {} or [] or '' = initialState
     songs: songsSlice.reducer,
+    movies: moviesSlice.reducer,
   }
 })
 
@@ -27,9 +42,6 @@ const store = configureStore({
 const startingState = store.getState();
 console.log(JSON.stringify(startingState))
 
-
-store.dispatch(
-  songsSlice.actions.addSong("New Song!!!")
-)
-
-console.log('actions2: ', store.getState())
+export { store };
+export const { addSong, removeSong} = songsSlice.actions;
+export const { addMovie, removeMovie } = moviesSlice.actions;
