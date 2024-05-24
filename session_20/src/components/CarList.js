0b58d2512539973d9ab0261ea697787abde94572
hank <>
2024-05-24 17:51:50 +0800

    feat: temporary storage

--- session_20/src/components/CarList.js
+++ session_20/src/components/CarList.js
@@ -1,6 +1,36 @@
+import { useSelector, useDispatch } from "react-redux";
+import { removeCar } from "../store";
+
 function CarList() {
+  const dispatch = useDispatch();
+  const cars = useSelector(state => {
+    return state.cars.data;
+  });
+  console.log(cars);
+
+  const handleCarDelete = car => {
+    dispatch(removeCar(car.id))
+  }
+
+  const renderedCars = cars.map(car => {
+    return (
+      <div key={car.id} className="car">
+        <p>{car.name} - ${car.cost}</p>
+        <button
+          className="button is-danger"
+          onClick={() => handleCarDelete(car)}
+        >
+          Delete
+        </button>
+      </div>
+    )
+  })
+
   return (
-    <div>CarList</div>
+    <div className="cars-list">
+      {renderedCars}
+      <hr/>
+    </div>
   )
 }
 
--- session_20/src/store/index.js
+++ session_20/src/store/index.js
@@ -1,6 +1,6 @@
-import { configureStore} from "@reduxjs/toolkit";
-import { carsReducer, changeSearchTerm, addCar, removeCar } from "./slices/carsSlice";
-import { formReducer, changeName, changeCost } from "./slices/formSlice";
+import { configureStore } from "@reduxjs/toolkit";
+import { carsReducer, changeSearchTerm, addCar, removeCar } from './slices/carsSlice';
+import { formReducer, changeName, changeCost } from './slices/formSlice';
 
 const store = configureStore({
   reducer: {
