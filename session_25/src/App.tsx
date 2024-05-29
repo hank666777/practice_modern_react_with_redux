adbaae98259c2e7669f879d9f231f4bcfd0daf5c
hank666777 <fengptt47@gmail.com>
2024-05-30 01:24:04 +0800

    feat: temporary storage

--- session_25/src/App.tsx
+++ session_25/src/App.tsx
@@ -2,34 +2,22 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Root from './pages/Root';
 import HomePage from "./pages/HomePage";
 import DetailsPage from "./pages/DetailsPage";
-import SearchPage from "./pages/SearchPage";
+import { searchLoader } from "./pages/search/searchLoader.ts";``
+import SearchPage from "./pages/search/SearchPage.tsx";
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <Root/>,
     children: [
-      {// 路由不匹配，默認值
+      {// 路由不匹配時，默認值
         index: true,
         element: <HomePage/>,
       },
       {
         path: '/search',
         element: <SearchPage/>,
-        loader: async ({ request }) => {
-          const { searchParams } = new URL(request.url);
-          const term = searchParams.get('term');
-          if (!term) {
-            throw new Error('search term must be provided.')
-          }
-
-          const res = await fetch(
-            `https://registry.npmjs.org/-/v1/search?text=${term}`,
-          )
-          const data = await res.json();
-
-          return data.objects;
-        },
+        loader: searchLoader,
       },
       {// /packages/任意名 訪問 DetailsPage
         path: '/packages/:name',
